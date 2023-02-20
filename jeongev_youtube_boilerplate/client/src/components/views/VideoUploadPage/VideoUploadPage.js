b64af23d18c3ed5f5aba87262ac1b1b8530038ea
jeongdev55 <wkdgowjdpo@gmail.com>
2023-02-21 00:20:12 +0900

    video upload page submit form 수정 +

--- jeongev_youtube_boilerplate/client/src/components/views/VideoUploadPage/VideoUploadPage.js
+++ jeongev_youtube_boilerplate/client/src/components/views/VideoUploadPage/VideoUploadPage.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React ,{useState} from 'react'
 import Dropzone from 'react-dropzone'
 import {Typography, Button, Form, message, Input, Icon} from 'antd';
 
@@ -6,7 +6,43 @@ import {Typography, Button, Form, message, Input, Icon} from 'antd';
 const {Title} = Typography;
 const {TextArea} = Input;
 
+const PrivateOptions =[
+  {value :0,label :"Private"},
+  {value :1,label :"Public"}
+]
+
+
+const CategoryOptions =[
+  {value :0,label :"Film & Animation"},
+  {value :1,label :"Autos & Vehicles"},
+  {value :2,label :"Music"},
+  {value :3,label :"Pets & Animals"},
+  {value :4,label :"Sports"}
+]
+
+
 function VideoUploadPage() {
+  const [VideoTitle, setVideoTitle] = useState("")
+  const [Description, setDescription] = useState("")
+  const [Private, setPrivate] = useState(0) //private =0 public =1
+  const [Category, setCategory] = useState("Film & Animation")
+
+
+  const onDescriptionChange=(e)=>{
+    setDescription(e.currentTarget.value)
+  }
+  const onTitleChange=(e)=>{
+    setVideoTitle(e.currentTarget.value)
+  }
+  
+  const onPrivateChange=(e)=>{
+    setPrivate(e.currentTarget.value)
+  }
+  
+  const onCategoryChange=(e)=>{
+    setCategory(e.currentTarget.value)
+  }
+
   return (
     <div style={{maxWidth:'700px', margin:'2rem auto'}}>
       <div style ={{textAlign:'center', marginBottom:'2rem'}}>
@@ -19,13 +55,13 @@ function VideoUploadPage() {
              onDrop
              multiple
              maxSize>
-             {({getRootProps,getInputPros})=>{
+             {({getRootProps,getInputProps})=>(
                <div style={{width:'300px',height:'240px',border:'1px solid lightgray',display:'flex',
-              alignItems:'center',justifyContent:'center'}}{...getRootProps()}>
-              <input {...getInputPros()} />
+              alignItems:'center',justifyContent:'center'}}{...getRootProps()} >
+              <input {...getInputProps()} />
               <Icon type="plus" style={{fontSize:'3rem' }}/>
               </div>
-             }}
+             )}
             </Dropzone>
 
              {/* Thumbnail*/}
@@ -37,21 +73,31 @@ function VideoUploadPage() {
         <br/>
         <label>Title</label>
         <Input
-          onChange
-          value 
+          onChange ={onTitleChange}
+          value ={VideoTitle}
         />
         <br />
         <br />
         <label>Description</label>
         <TextArea
-          onChange
-          value 
+          onChange ={onDescriptionChange}
+          value={Description}
         />
         <br />
         <br />
-        <select onChange>
-          <option key value></option>
+        <select onChange={onPrivateChange}>
+          {PrivateOptions.map((item,index)=>(
+             <option key={index} value={item.value}>{item.label}</option>
+          ))}
         </select>
+        <br />
+        <br />
+        <select onChange={onCategoryChange}>
+          {CategoryOptions.map((item,index)=>(
+             <option key={index} value={item.value}>{item.label}</option>
+          ))}
+        </select>
+        <br />
         <br />
         <Button type="primary" size="large" onClick>
           Submit
